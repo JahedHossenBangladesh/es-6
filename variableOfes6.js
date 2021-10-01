4bb14b32237573c4b01a8e58855f5ddf5e7ca55d
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-10-01 10:09:13 +0600

    This is es6 1:24

---
+++ variableOfes6.js
@@ -0,0 +1,13 @@
+// let const and var
+const number =20 ;
+let name;
+const compire =number > 1 ?  name ='John': name = 'sara';
+
+console.log(compire)  //John
+
+function functionBlock (){
+    var age = 50;
+}
+
+functionBlock();
+console.log(age) //undefined 
